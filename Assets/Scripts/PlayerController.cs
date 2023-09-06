dcec5055fb7c9ff7a6bb83f6935114f6768088b9
John Creech <johnwaynecreech1@gmail.com>
2023-09-06 17:41:56 -0400

    Starting on Character and Floor

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float xJump = 1.0f;
+    public float yJump = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
