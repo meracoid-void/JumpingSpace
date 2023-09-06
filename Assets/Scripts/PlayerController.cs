b5bcfa8c3adceddec5e887f9e3018a1fa8d9fa6d
John Creech <johnwaynecreech1@gmail.com>
2023-09-06 18:23:10 -0400

    Player motion added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,18 +4,41 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float xJump = 1.0f;
-    public float yJump = 1.0f;
+    public float speed = 5.0f;  // Movement speed
+    public float jumpForce = 5.0f;  // Jump force
+    private Rigidbody2D rb;     // Reference to the Rigidbody2D component
+    private bool isJumping = false; // Flag to check if the player is currently jumping
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Get the Rigidbody2D component attached to this GameObject
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Get horizontal input only
+        float horizontal = Input.GetAxis("Horizontal");
+
+        // Create a 2D movement vector for the horizontal direction
+        Vector2 movement = new Vector2(horizontal, rb.velocity.y);
+
+        // Apply movement to the Rigidbody2D
+        rb.velocity = movement * new Vector2(speed, 1);
+
+        // Check for jump input
+        if (Input.GetButtonDown("Jump") && !isJumping)
+        {
+            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            isJumping = true;
+        }
+    }
+
+    // OnCollisionEnter2D is called when this collider/rigidbody has begun touching another rigidbody/collider
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Reset the isJumping flag when player collides with ground
+        isJumping = false;
     }
 }
